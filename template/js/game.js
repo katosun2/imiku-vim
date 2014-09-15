876d09156f421e19498ef28f7e855b31176658ab
katosun2 <katosun2@gmail.com>
2014-09-15 14:52:06 +0800

    update phaser game tpl

--- template/js/game.js
+++ template/js/game.js
@@ -1,14 +1,89 @@
-/*加载资源*/
-var preload = function() { };
+//定义一个游戏
+var game = new Phaser.Game(288, 505, Phaser.AUTO, 'mygame');
 
-/*创建世界*/
-var create = function() { };
+/*定义游戏场景*/
+game.States = {
+	//boot场景，用来做一些游戏启动前的准备
+	boot : function(){ 
+		this.preload = function(){
+			game.load.image('loading', './img/preloader.gif');
+		};
+		this.create = function(){
+			//启动资源加载
+			game.state.start('preload');
+			//game.world.setBounds(0, 0, 4000, 300);
+		};
+	},
+	//prelaod场景，用来显示资源加载进度
+	preload : function(){
+		this.preload = function(){
+			/*var preloadSprite = game.add.sprite( game.world.centerX, game.world.centerY, 'loading');
+			preloadSprite.anchor.set(0.5,0);
+			game.load.setPreloadSprite(preloadSprite, 0);*/
 
-/*每帧重绘*/
-var update = function() { };
+			//加载进度
+			var text = game.add.text( game.world.centerX, game.world.centerY, "0%",{font:"16px Arial", fill:"#FFFFFF", align: "center"}),
+			percent = 0,
+			curPercent = 0,
+			loadKey,
+			_addPrecent = function(from, to){
+				if(percent < to){
+					if(percent >= from){
+						percent++;
+					}
 
-/*每帧渲染*/
-var render = function() { };
+					if (percent > 100) {
+						percent = 100;
+					}
 
-/*第三个参数可以是Phaser.CANVAS，Phaser.WEBGL，或Phaser.AUTO。这个是你想使用的渲染上下文。推荐的参数是Phaser.AUTO，它会自动尝试使用WebGL，如果浏览器或设备不支持它就会回滚成Canvas。*/
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '空|id|dom', { preload: preload, create: create, update: update, render: render}, true);
+					text.setText(percent + "%");
+
+					if(percent == 100){
+						game.time.events.remove(loadKey);
+					}
+				}
+			};
+			//文字居中
+			text.anchor.set(0.5, 0.5);
+			//添加定时器
+			loadKey = game.time.events.loop(Phaser.Timer.SECOND / 30, function(){
+				_addPrecent(percent , curPercent);
+			}, this);
+			//绑定
+			game.load.onFileComplete.add(function( progress ) {
+				curPercent  = progress;
+			});
+
+			//加载资源
+		};
+		this.create = function(){
+			//启动菜单场景
+			game.state.start('menu');
+		};
+	},
+	//menu场景, 显示菜单
+	menu : function(){
+		this.create = function(){ };
+	},
+	//play场景， 游戏场景
+	play : function(){
+		this.create = function(){ };
+		this.update = function(){ };
+	}
+};
+
+game.main = {
+	init : function(){
+		//添加场景
+		game.state.add('boot', game.States.boot);
+		game.state.add('preload', game.States.preload);
+		game.state.add('menu', game.States.menu);
+		game.state.add('play', game.States.play);
+
+		//调用boot启动游戏
+		game.state.start('boot');
+	}
+};
+
+/*初始化*/
+game.main.init();
